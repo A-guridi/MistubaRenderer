90be6a2c75f12b9666fad5fc26145af2454b836c
ge93sut <ge93sut@mytum.de>
2021-11-28 23:59:20 +0100

    Added complex scene 6

--- Mitsuba2/main_mitsuba.py
+++ Mitsuba2/main_mitsuba.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         "/home/arturo/PycharmProjects/MistubaRenderer/resources/cctextures/Wood001/Wood001_2K_Color.jpg",
         "/home/arturo/PycharmProjects/MistubaRenderer/resources/cctextures/Tiles023/Tiles023_2K_Color.jpg"]
     res_x, res_y = 612, 512
-    spp = 512
+    spp = 256
     starting_number = 0
     RenderObj = Renderer(output_dir=out_path, scene=scene, camera_file=camera_file, res_x=res_x, res_y=res_y,
                          spp=spp, floor_textures=floor_textures, starting_number=starting_number)
