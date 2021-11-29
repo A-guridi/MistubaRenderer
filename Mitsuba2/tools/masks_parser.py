9b57a4cfe0aca668dc35202d36de1403f0291611
ge93sut <ge93sut@mytum.de>
2021-11-29 20:20:42 +0100

    Added complex scene 9

--- Mitsuba2/tools/masks_parser.py
+++ Mitsuba2/tools/masks_parser.py
@@ -14,7 +14,7 @@ import shutil
 
 class MasksParser:
 
-    def __init__(self, files_path, ann_file, file_format=".png", class_id=None):
+    def __init__(self, files_path, ann_file, file_format=".png", class_id=None, def_size=(512, 512)):
         self.files_path = files_path
         self.ann_file = ann_file
         self.out_path = self.files_path + f"/coco_masks/"
@@ -27,6 +27,9 @@ class MasksParser:
         self.file_format = file_format
         self.coco = COCO(self.ann_file)
         self.ids = self.create_ids(class_id)
+        cat_ids = self.coco.getCatIds()
+        self.num_files = len(self.coco.getImgIds(catIds=cat_ids))
+        self.def_size = def_size
 
     def create_ids(self, class_id):
         if class_id is None:
@@ -60,10 +63,16 @@ class MasksParser:
             # def_image = np.where(def_image == 0, 255, 0)
             plt.imsave(self.out_path + str(img['id']) + self.file_format, def_image, cmap="gray")
 
+        if len(img_ids) != self.num_files:
+            for i in range(self.num_files):
+                if not os.path.isfile(self.out_path + str(i) + self.file_format):
+                    plt.imsave(self.out_path + str(i) + self.file_format, np.zeros(self.def_size), cmap="gray")
+
 
 if __name__ == "__main__":
     files_path = "/home/arturo/renders/complexscene/output/coco_data"
     ann_file = files_path + "/coco_annotations.json"
     class_id = [1, 333]  # 1 for the cup, 2 for the beer glass
-    masks_parser = MasksParser(files_path=files_path, ann_file=ann_file, class_id=class_id)
+    default_size = (612, 512)
+    masks_parser = MasksParser(files_path=files_path, ann_file=ann_file, class_id=class_id, def_size=default_size)
     masks_parser.save_all_masks()
